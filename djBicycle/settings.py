6155b1ef52474006de1c4512503b144eb32c6006
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-13 21:52:14 -0500

    fix

--- djBicycle/settings.py
+++ djBicycle/settings.py
@@ -118,8 +118,8 @@ REST_FRAMEWORK = {
    ),
 
    'DEFAULT_PERMISSION_CLASSES': (
-       'rest_framework.permissions.IsAuthenticated',
-       'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+       # 'rest_framework.permissions.IsAuthenticated',
+       # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
    ),
    'DEFAULT_FILTER_BACKENDS': (
        'rest_framework.filters.DjangoFilterBackend',
